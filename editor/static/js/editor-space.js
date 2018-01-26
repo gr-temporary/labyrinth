07b9aedaa2307d48286fca36fa20d66285056a5e
Grigoriy Kutarev <gk@mactelabs.com>
2018-01-26 19:39:29 +0700

    extract viewport

--- editor/static/js/editor-space.js
+++ editor/static/js/editor-space.js
@@ -7,84 +7,6 @@
 	const HIGHLIGHT = 2;
 	const MOVING = 3;
 
-	const Transform = function() {
-		this.x = 0;
-		this.y = 0;
-		this.scale = 1;
-	}
-
-	const Rect = function() {
-		this.left = 0;
-		this.right = 0;
-		this.top = 0;
-		this.bottom = 0;
-		this.width = 0;
-		this.height = 0;
-	}
-
-	Rect.prototype.update = function (w, h, x, y, scale) {
-		let w2 = w / (2 * scale);
-		let h2 = h / (2 * scale);
-		this.left = x - w2;
-		this.right = x + w2;
-		this.top = y - h2;
-		this.bottom = y + h2;
-		this.width = 2 * w2;
-		this.height = 2 * h2;
-	}
-
-	const Renderer = function(context, viewport, camera) {
-		this.context = context;
-		this.viewport = viewport;
-		this.camera = camera;
-		this.update();
-	}
-
-	Renderer.prototype.update = function () {
-		this.width = this.context.canvas.width;
-		this.height = this.context.canvas.height;
-		this.scaleX = this.width / this.viewport.width;
-		this.scaleY = this.height / this.viewport.height;
-	}
-
-	Renderer.prototype.getX = Renderer.prototype.worldToScreenX = function (x) {
-		return this.scaleX * x - this.camera.x + this.width / 2;
-	}
-
-	Renderer.prototype.getY = Renderer.prototype.worldToScreenX = function (y) {
-		return this.scaleY * y - this.camera.y + this.height / 2;
-	}
-
-	Renderer.prototype.screenToWorldX = function (x) {
-		return this.viewport.left + (x / this.width) * this.viewport.width;
-	}
-
-	Renderer.prototype.screenToWorldY = function (y) {
-		return this.viewport.top + (y / this.height) * this.viewport.height;
-	}
-
-	Renderer.prototype.clear = function (color) {
-		let ctx = this.context;
-		ctx.fillStyle = color || "#aaa";
-		ctx.fillRect(0, 0, this.width, this.height);
-	}
-
-	Renderer.prototype.beginPath = function () {
-		this.context.beginPath();
-	}
-
-	Renderer.prototype.moveTo = function (x, y) {
-		x = this.getX(x);
-		y = this.getY(y);
-		this.context.moveTo(x, y);
-	}
-
-	Renderer.prototype.lineTo = function (x, y) {
-		x = this.getX(x);
-		y = this.getY(y);
-		this.context.lineTo(x, y);
-	}
-
 	let leveldata = {
 		rooms: {},
 		doors: {}
@@ -94,28 +16,14 @@
 		template: '#level-editor',
 		data: function() {
 			return {
-				context: null,
-				screenWidth: 0,
-				screenHeight: 0,
-				zoom: 10,
-				camera: new Transform(),
-				viewport: new Rect(),
 				renderer: null
 			};
 		},
 		methods: {
-			updateSize: function() {
-				this.screenWidth = this.$refs.screen.offsetWidth;
-				this.screenHeight = this.$refs.screen.offsetHeight;
-				this.$refs.canvas.width = this.screenWidth
-				this.$refs.canvas.height = this.screenHeight;
-			},
 			draw: function() {
-				if(!this.context) {
+				if(!this.renderer) {
 					return;
 				}
-				this.viewport.update(this.screenWidth, this.screenHeight, this.camera.x, this.camera.y, this.camera.scale);
-				this.renderer.update();
 
 				this.renderer.clear();
 
@@ -123,44 +31,55 @@
 			},		
 			drawGrid: function() {
 
-				let ctx = this.context;
+				let ctx = this.renderer.context;
+				let viewport = this.renderer.viewport;
 
 				ctx.strokeStyle = '#eee';
 				ctx.lineWidth = 1;
 
-				let start = this.viewport.left / UNIT | 0;
-				let end = this.viewport.right / UNIT | 0;
+				let start = viewport.left / UNIT | 0;
+				let end = viewport.right / UNIT | 0;
 
 				ctx.beginPath();
 				for(let i=start; i<=end; i++) {
 					let x = i * UNIT;
-					this.renderer.moveTo(x, this.viewport.top);
-					this.renderer.lineTo(x, this.viewport.bottom);
+					this.renderer.moveTo(x, viewport.top);
+					this.renderer.lineTo(x, viewport.bottom);
 				}
 
-				start = this.viewport.top / UNIT | 0;
-				end = this.viewport.bottom / UNIT | 0;
+				start = viewport.top / UNIT | 0;
+				end = viewport.bottom / UNIT | 0;
 				for(let i=start; i<=end; i++) {
 					let y = i * UNIT;
-					this.renderer.moveTo(this.viewport.left, y);
-					this.renderer.lineTo(this.viewport.right, y);
+					this.renderer.moveTo(viewport.left, y);
+					this.renderer.lineTo(viewport.right, y);
 				}
 
 				ctx.stroke();
-			},
-			addZoom: function(step) {
-				this.zoom += step;
-				this.camera.scale = this.zoom * this.zoom / 100;
-				this.draw();
+
+				ctx.lineWidth = 3;
+				ctx.beginPath();
+				this.renderer.moveTo(0, viewport.top);
+				this.renderer.lineTo(0, viewport.bottom);
+				this.renderer.moveTo(viewport.left, 0);
+				this.renderer.lineTo(viewport.right, 0);
+				ctx.stroke();
+
+				ctx.strokeStyle = "#f00";
+				this.renderer.strokeRect(viewport.left, viewport.top, UNIT, UNIT);
+				this.renderer.strokeRect(viewport.right - UNIT / this.renderer.scale.x, viewport.top, UNIT, UNIT);
+				this.renderer.strokeRect(viewport.right - UNIT / this.renderer.scale.x, viewport.bottom - UNIT / this.renderer.scale.y, UNIT, UNIT);
+				this.renderer.strokeRect(viewport.left, viewport.bottom - UNIT / this.renderer.scale.y, UNIT, UNIT);
 			},
 			setLevelData: function(data) {
 				console.log("Data set!");
 			}
 		},
 		mounted: function() {
-			this.context = this.$refs.canvas.getContext("2d");
-			this.renderer = new Renderer(this.context, this.viewport, this.camera);
-			this.updateSize();
+			this.renderer = this.$refs.viewport;
+
+			console.log("Editor mounted");
+
 			this.draw();
 		}
 	});
---
+++ editor/static/js/viewport.js
@@ -0,0 +1,216 @@
+const Rect = function(x, y, w, h) {
+	x = x || 0;
+	y = y || 0;
+	w = w || 0;
+	h = h || 0;
+	this.left = x - w / 2;
+	this.right = x + w / 2;
+	this.top = y - h / 2;
+	this.bottom = y + h / 2;
+	this.width = w;
+	this.height = h;
+}
+
+Rect.prototype.update = function (size, position, scale) {
+	let w2 = size.width / (2 * scale.x);
+	let h2 = size.height / (2 * scale.y);
+	this.left = position.x - w2;
+	this.right = position.x + w2;
+	this.top = position.y - h2;
+	this.bottom = position.y + h2;
+	this.width = 2 * w2;
+	this.height = 2 * h2;
+}
+
+Rect.prototype.contains = function (x, y) {
+	return x >= this.left && x <= this.right && y >= this.top && y <= this.bottom;
+}
+
+Vue.component('editor-viewport', {
+	template: '#editor-viewport',
+	data: function() {
+		return {
+			context: null,
+			position: { // units
+				x: 0,
+				y: 0
+			},
+			zoom: 10,
+			maxZoom: 20,
+			minZoom: 1,
+			zoomScale: {
+				x: 1,
+				y: 1
+			},
+			scale: {
+				x: 1,
+				y: 1
+			},
+			viewport: new Rect(), // units
+			size: { // pixels
+				width: 0,
+				height: 0
+			},
+			cursorPosition: { // pixels
+				x: 0,
+				y: 0
+			},
+			prevCursorPosition: { // pixels
+				x: 0,
+				y: 0
+			},
+			cursorDelta: { // pixels
+				x: 0,
+				y: 0
+			},
+			mouseDown: false
+		}
+	},
+	methods: {
+		updateSize: function() {
+			this.size.width = this.$refs.screen.offsetWidth;
+			this.size.height = this.$refs.screen.offsetHeight;
+			this.updateViewport();
+
+			this.$refs.canvas.width = this.size.width
+			this.$refs.canvas.height = this.size.height;
+
+			this.needRedraw();
+		},
+		updateViewport: function() {
+			this.viewport.update(this.size, this.position, this.zoomScale);
+			this.scale.x = this.size.width / this.viewport.width;
+			this.scale.y = this.size.height / this.viewport.height;
+		},
+		addZoom: function(step) {
+			this.zoom += step;
+			if(this.zoom > this.maxZoom) this.zoom = this.maxZoom;
+			if(this.zoom < this.minZoom) this.zoom = this.minZoom;
+			this.zoomScale.x = this.zoomScale.y = this.zoom * this.zoom / 100;
+			this.updateViewport();
+			this.needRedraw();
+		},
+		resetViewport: function() {
+			this.position.x = 0;
+			this.position.y = 0;
+			this.zoom = (this.maxZoom + this.minZoom) / 2 | 0;
+			this.zoomScale.x = this.zoomScale.y = this.zoom * this.zoom / 100;
+			this.updateViewport();
+			this.needRedraw();
+		},
+		contains: function(x, y) {
+			return this.viewport.contains(x, y);
+		},
+		needRedraw: function() {
+			this.$emit("render");
+		},
+
+		clear: function(color) {
+			let ctx = this.context;
+			ctx.fillStyle = color || "#aaa";
+			ctx.fillRect(0, 0, this.size.width, this.size.height);
+		},
+		beginPath: function() {
+			this.context.beginPath();
+		},
+		stroke: function() {
+			this.context.stroke();
+		},
+		moveTo: function(x, y) {
+			x = this.getX(x);
+			y = this.getY(y);
+			this.context.moveTo(x, y);
+		},
+		lineTo: function(x, y) {
+			x = this.getX(x);
+			y = this.getY(y);
+			this.context.lineTo(x, y);
+		},
+		strokeRect: function(x, y, w, h, hud) {
+			x = this.getX(x);
+			y = this.getY(y);
+			this.context.strokeRect(x, y, w, h);
+		},
+
+		getX: function(x) {
+			return this.scale.x * (x - this.position.x) + this.size.width / 2;
+		},
+		getY: function(y) {
+			return this.scale.y * (y - this.position.y) + this.size.height / 2;
+		},
+		screenToWorldX: function (x) {
+			return this.viewport.left + (x / this.size.width) * this.viewport.width;
+		},
+		screenToWorldY: function (y) {
+			return this.viewport.top + (y / this.size.height) * this.viewport.height;
+		},
+
+		onMouseMove: function(event) {
+			//console.log(event);
+			let x = event.offsetX;
+			let y = event.offsetY;
+
+			this.prevCursorPosition.x = this.cursorPosition.x;
+			this.prevCursorPosition.y = this.cursorPosition.y;
+
+			this.cursorPosition.x = x;
+			this.cursorPosition.y = y;
+
+			this.cursorDelta.x = x - this.prevCursorPosition.x;
+			this.cursorDelta.y = y - this.prevCursorPosition.y;
+
+			if(this.mouseDown) {
+				this.position.x -= this.cursorDelta.x / this.scale.x;
+				this.position.y -= this.cursorDelta.y / this.scale.y;
+				this.updateViewport();
+				this.needRedraw();
+			}
+		},
+		onMouseDown: function(event) {
+			this.mouseDown = true;
+			//console.log(event);
+			let x = event.offsetX;
+			let y = event.offsetY;
+			this.cursorPosition.x = this.prevCursorPosition.x = x;
+			this.cursorPosition.y = this.prevCursorPosition.y = y;
+			this.$emit("mousedown", {
+				screenX: x,
+				screenY: y,
+				x: this.screenToWorldX(x),
+				y: this.screenToWorldY(y)
+			});
+		},
+		onMouseUp: function(event) {
+			this.mouseDown = false;
+			console.log(event);
+			this.$emit("mouseup", {
+				screenX: event.offsetX,
+				screenY: event.offsetY,
+				x: this.screenToWorldX(event.offsetX),
+				y: this.screenToWorldY(event.offsetY)
+			});
+		}
+	},
+	mounted: function() {
+		this.context = this.$refs.canvas.getContext("2d");
+		this.updateSize();
+
+		this.clear("#aaa");
+
+		console.log("Viewport mounted");
+		
+		window.addEventListener("resize", e => {
+			this.updateSize();
+		});
+
+		this.$refs.canvas.addEventListener("mousemove", e => {
+			this.onMouseMove(e);
+		});
+		this.$refs.canvas.addEventListener("mousedown", e => {
+			this.onMouseDown(e);
+		});
+		this.$refs.canvas.addEventListener("mouseup", e => {
+			this.onMouseUp(e);
+		});
+	}
+});
